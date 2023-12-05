8e89b2214011bb71d8411785dae420e3f8cf1771
sherlly <jacob@sherllymail.com>
2023-12-05 22:03:22 +0000

    code init

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const PASSWORD = "052"
+
+function checkWin() {
+    var userGuess = document.getElementById('passbox').value;
+    if (userGuess === PASSWORD) {
+        document.getElementById('page').innerHTML = "<h1>Well done, you solved the puzzle</h1><br><p>The puzzle was taken from <a href='https://www.amazon.co.uk/Sherlock-Holmes-Mind-Palace-Puzzles/dp/1787395537/ref=sr_1_1?keywords=mind+palace+puzzles'>Mind Palace Puzzles</a>, which contains within it the best tutorial on becoming an expert on mind palaces as well as a lot of puzzles. I highly recommend it</p><br><p>Now it's time for your actual present </p>";
+    }
+    else {
+        document.getElementById('passbox').style.borderColor = "red";
+        document.getElementById('passbox').value = "";
+    }
+}
+
